61b1031396e3ad6f4af684ef9f91f7907c91b8e5
Nabin Budhathoki <tusharbcbudhathoki@gmail.com>
2025-11-28 17:37:09 +0545

    small fix

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/Product-Listing-Page",
+  base: "/Product-Listing-Page/",
   plugins: [
     react(),
     tailwindcss(),
